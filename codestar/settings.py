0d54c9edb93452fdb6a3cb4d7617b68eed4258ea
Inna-Kot <kotkovets.dev@outlook.ie>
2025-03-20 10:46:06 +0000

    import packages to settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
